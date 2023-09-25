bff2b4dda9a8be2c81c79bc0b3367e6f0588f237
luis-moguea <luis.mogueadp@gmail.com>
2023-09-24 21:58:38 -0500

    delete unnecessary imports

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
 import MapScreen from './MapScreen';
 
 const Stack = createStackNavigator();
